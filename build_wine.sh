06e31a27161b554dc93f7cf38d09982a5b1b4c14
akhavr <akhavr@khavr.com>
2017-03-20 12:05:28 +0200

    Use electrum-dash .spec and .nsi
    Fixed deterministic.spec and electrum-dash.nsi were merged electrum-dash
    upstream

--- build_wine.sh
+++ build_wine.sh
@@ -3,7 +3,7 @@ wineboot && sleep 5
 wineboot --update
 
 cp /root/.wine/drive_c/Python27/Lib/site-packages/requests/cacert.pem .
-wine python "C:/PyInstaller/pyinstaller.py" -y "../deterministic.spec"
-cp /opt/electrum-dash.nsi /root/.wine/drive_c/
+wine python "C:/PyInstaller/pyinstaller.py" -y "contrib/build-wine/deterministic.spec"
+cp /opt/electrum-dash/contrib/build-wine/electrum-dash.nsi /root/.wine/drive_c/
 cd /root/.wine/drive_c/electrum
 wine c:\\"Program Files (x86)"\\NSIS\\makensis.exe -V1 c:\\electrum-dash.nsi
