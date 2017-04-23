f3af7d9bd594d2de58c6232fdbf07b309ed3e31a
akhavr <akhavr@khavr.com>
2017-04-23 21:13:03 +0300

    Get details of the installed PyQt package

--- travis-build-osx.sh
+++ travis-build-osx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone https://github.com/dashpay/electrum-dash.git
-find / -iname pyrcc4
+brew info PyQt
 pyrcc4 icons.qrc -o gui/qt/icons_rc.py
 python setup.py sdist --format=zip,gztar
