f05b298e0be3a494bea8bfbca0260f2cc09c7e00
akhavr <akhavr@khavr.com>
2017-04-27 14:41:44 +0300

    Try antitimeout hack for travis-ci

--- install_pyqt4_osx.sh
+++ install_pyqt4_osx.sh
@@ -5,5 +5,12 @@ tar xzf PyQt4_gpl_x11-4.12.tar.gz
 cd PyQt4_gpl_x11-4.12
 which qmake
 python configure.py --verbose --confirm-license --no-designer-plugin --no-qsci-api --no-timestamp
-make -j2
+make -j4 &> /dev/null &
+pid=$!
+while kill -0 $pid 2>/dev/null
+do
+    echo -ne '.'
+    sleep 1
+done
 make install
+echo -ne "PyQt done"
