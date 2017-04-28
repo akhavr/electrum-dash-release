1033acd19f7be84f56f7f2a347160690af81660e
akhavr <akhavr@khavr.com>
2017-04-28 12:16:31 +0300

    Comment out SIP and PyQt build instructions
    To get Qt built

--- before_install-osx.sh
+++ before_install-osx.sh
@@ -4,10 +4,10 @@ brew update
 if [ "$(which qmake)" == "" ]; then
     ./install_qt_osx.sh
 fi
-if [ "$(which sip)" == "" ]; then
-    ./install_sip_osx.sh
-fi
-if [ "$(which pyrcc4)" == "" ]; then
-    ./install_pyqt4_osx.sh
-fi
-pip install --upgrade pip
+# if [ "$(which sip)" == "" ]; then
+#     ./install_sip_osx.sh
+# fi
+# if [ "$(which pyrcc4)" == "" ]; then
+#     ./install_pyqt4_osx.sh
+# fi
+# pip install --upgrade pip
