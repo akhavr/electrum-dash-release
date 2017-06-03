164470d46347df67379b9b2af854fe87355fed0c
akhavr <akhavr@khavr.com>
2017-06-03 13:51:40 +0300

    Use cartr/qt4/pyqt@4 brew package

--- before_install-osx.sh
+++ before_install-osx.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 set -ev
 brew update
-if [ "$(which qmake)" == "" ]; then
-    ./install_qt_osx.sh
-fi
-# if [ "$(which sip)" == "" ]; then
-#     ./install_sip_osx.sh
-# fi
-# if [ "$(which pyrcc4)" == "" ]; then
-#     ./install_pyqt4_osx.sh
-# fi
-# pip install --upgrade pip
+brew install cartr/qt4/pyqt@4
+pip install --upgrade pip
