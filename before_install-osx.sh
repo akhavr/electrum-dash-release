376e7f59ecbff35932f23254b8f8c88405906a27
akhavr <akhavr@khavr.com>
2017-06-03 14:08:53 +0300

    Install necessary packages and build electrum-dash
    Packages: cython-hidapi, python-trezor, pyinstaller

--- before_install-osx.sh
+++ before_install-osx.sh
@@ -3,3 +3,12 @@ set -ev
 brew update
 brew install cartr/qt4/pyqt@4
 pip install --upgrade pip
+
+git clone  https://github.com/trezor/cython-hidapi
+(cd cython-hidapi; git pull; git submodule init; git submodule update)
+(cd cython-hidapi/; python setup.py build install)
+
+git clone https://github.com/akhavr/python-trezor
+(cd python-trezor/ ; sudo python setup.py build install bdist)
+
+pip install pyinstaller
