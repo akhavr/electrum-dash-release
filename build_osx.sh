c4c9baeb574db3a4e0e4181bfde51e7a297fa5e4
akhavr <akhavr@khavr.com>
2017-06-11 21:22:49 +0300

    Find out where's `trezorctl.py` at travis build

--- build_osx.sh
+++ build_osx.sh
@@ -3,6 +3,5 @@ cd electrum-dash
 pyrcc4 icons.qrc -o gui/qt/icons_rc.py
 python setup.py sdist --format=zip,gztar
 mv contrib/osx.spec .
-pwd
-ls
+find . -name trezorctl.py
 pyinstaller -y --clean osx.spec
