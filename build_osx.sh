62765159848f92b1622d4ea05942e2b41699636f
akhavr <akhavr@khavr.com>
2017-06-11 21:52:34 +0300

    Orient on the file system

--- build_osx.sh
+++ build_osx.sh
@@ -3,5 +3,10 @@ cd electrum-dash
 pyrcc4 icons.qrc -o gui/qt/icons_rc.py
 python setup.py sdist --format=zip,gztar
 mv contrib/osx.spec .
+
+echo -----------------
 find .. -name trezorctl.py
+pwd
+ls ..
+ls 
 pyinstaller -y --clean osx.spec
