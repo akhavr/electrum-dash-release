2978908a311ff8c56b7d1d0f0ad6149ad9a57fbc
zebra-lucky <zebra.lucky@gmail.com>
2017-08-29 11:58:34 +0300

    add build matrix for linux/osx, change repo for linux build

--- build_wine.sh
+++ build_wine.sh
@@ -2,7 +2,7 @@
 wineboot && sleep 5
 wineboot --update
 
-cp /root/.wine/drive_c/Python27/Lib/site-packages/requests/cacert.pem .
+cp /root/.wine/drive_c/Python27/Lib/site-packages/pip/_vendor/requests/cacert.pem .
 wine python "C:/PyInstaller/pyinstaller.py" -y "contrib/build-wine/deterministic.spec"
 cp /opt/electrum-dash/contrib/build-wine/electrum-dash.nsi /root/.wine/drive_c/
 cd /root/.wine/drive_c/electrum
