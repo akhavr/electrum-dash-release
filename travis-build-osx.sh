f28ec87141ce629d5be57fd9268797001db73f8e
zebra-lucky <zebra.lucky@gmail.com>
2017-09-01 22:20:14 +0300

    add build matrix for linux/osx, change repo for linux build

--- travis-build-osx.sh
+++ travis-build-osx.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-git clone https://github.com/dashpay/electrum-dash.git
-brew info PyQt
+git clone https://github.com/akhavr/electrum-dash.git electrum-dash
+cd electrum-dash
 pyrcc4 icons.qrc -o gui/qt/icons_rc.py
 python setup.py sdist --format=zip,gztar
+mv contrib/osx.spec .
+mkdir -p packages/requests
+cp /usr/local/lib/python2.7/site-packages/pip/_vendor/requests/cacert.pem packages/requests/cacert.pem
+cp ../python-trezor/build/scripts-2.7/trezorctl packages/trezorctl.py
+pyinstaller -y --clean osx.spec
