b5bf8625a391e7f10e5e40ca62cf813e6a414fc7
zebra-lucky <zebra.lucky@gmail.com>
2017-09-21 15:31:29 +0300

    python2, install requirements, pyinstaller==3.3, default xcode

--- travis-build-osx.sh
+++ travis-build-osx.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 git clone https://github.com/akhavr/electrum-dash.git electrum-dash
 cd electrum-dash
+
+protobuf_path=/usr/local/lib/python2.7/site-packages/google
+sudo mkdir -p $protobuf_path
+sudo touch $protobuf_path/__init__.py
+sudo pip2 install protobuf==2.6.1
+
+sudo pip2 install \
+    dnspython==1.12.0 \
+    slowaes==0.1a1 \
+    ecdsa==0.13 \
+    requests==2.5.1 \
+    six==1.11.0 \
+    qrcode==5.1 \
+    pbkdf2==1.3 \
+    jsonrpclib==0.1.7 \
+    x11_hash>=1.4
+
 pyrcc4 icons.qrc -o gui/qt/icons_rc.py
-python setup.py sdist --format=zip,gztar
+python2 setup.py sdist --format=zip,gztar
+
 mv contrib/osx.spec .
 mkdir -p packages/requests
-cp /usr/local/lib/python2.7/site-packages/pip/_vendor/requests/cacert.pem packages/requests/cacert.pem
+cp /usr/local/lib/python2.7/site-packages/requests/cacert.pem packages/requests/
+
 cp ../python-trezor/build/scripts-2.7/trezorctl packages/trezorctl.py
+
 pyinstaller -y --clean osx.spec
-sudo hdiutil create -fs HFS+ -volname "Electrum-DASH" -srcfolder dist/Electrum-DASH.app dist/electrum-dash-macosx.dmg
+
+sudo hdiutil create -fs HFS+ -volname "Electrum-DASH" \
+    -srcfolder dist/Electrum-DASH.app dist/electrum-dash-macosx.dmg
