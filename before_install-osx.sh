f02977a9388e703519bfd7fab2b5a4fa3d7904aa
zebra-lucky <zebra.lucky@gmail.com>
2017-09-21 12:00:29 +0300

    add sudo to pip commands (#4)

--- before_install-osx.sh
+++ before_install-osx.sh
@@ -2,7 +2,7 @@
 set -ev
 brew update
 brew install cartr/qt4/pyqt@4
-pip2 install Cython
+sudo pip2 install Cython
 
 git clone  https://github.com/trezor/cython-hidapi
 (cd cython-hidapi; git pull; git submodule init; git submodule update)
@@ -11,4 +11,4 @@ git clone  https://github.com/trezor/cython-hidapi
 git clone https://github.com/akhavr/python-trezor
 (cd python-trezor/ ; sudo python setup.py build install bdist)
 
-pip2 install pyinstaller
+sudo pip2 install pyinstaller
